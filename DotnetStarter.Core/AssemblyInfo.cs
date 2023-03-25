4bb5e3a584ca2a9aa86107c607472d021f83dabc
Kristo Lippur <kristo@elnino.tech>
2023-03-25 23:17:27 +0100

    Update packages

--- DotnetStarter.Core/AssemblyInfo.cs
+++ DotnetStarter.Core/AssemblyInfo.cs
@@ -18,23 +18,6 @@ global using Mapster;
 global using MoreLinq.Extensions;
 using System.Reflection;
 
-[assembly: AssemblyVersion(
-	ThisAssembly.Git.BaseVersion.Major +
-	"." +
-	ThisAssembly.Git.BaseVersion.Minor +
-	"." +
-	ThisAssembly.Git.BaseVersion.Patch
-)]
-[assembly: AssemblyFileVersion(
-	ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch
-)]
-[assembly: AssemblyInformationalVersion(
-	ThisAssembly.Git.Commits +
-	"-" +
-	ThisAssembly.Git.Branch +
-	"+" +
-	ThisAssembly.Git.Commit
-)]
 [assembly: AssemblyCompany("Cloudey")]
 [assembly: AssemblyProduct("DotnetStarter")]
 [assembly: AssemblyTitle("DotnetStarter.Core")]
