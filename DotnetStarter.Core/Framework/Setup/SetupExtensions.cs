639b273f28413f32630c87869badeef28cfd3bda
Kristo Lippur <kristo@elnino.tech>
2023-04-23 22:42:50 +0200

    Simplify basic setup

--- DotnetStarter.Core/Framework/Setup/SetupExtensions.cs
+++ DotnetStarter.Core/Framework/Setup/SetupExtensions.cs
@@ -1,16 +1,10 @@
 using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using Cloudey.Reflex.Core;
-using Cloudey.Reflex.Core.Configuration;
-using Cloudey.Reflex.Core.Routing;
 using Cloudey.Reflex.Core.Setup;
 using Cloudey.Reflex.Database;
-using DotnetStarter.Core.Framework.Cache;
 using DotnetStarter.Core.Framework.Database;
 using DotnetStarter.Core.Framework.GraphQl;
 using DotnetStarter.Core.Framework.Identity;
-using DotnetStarter.Core.Framework.Validation;
-using FluentValidation.AspNetCore;
 using Microsoft.OpenApi.Models;
 using Opw.HttpExceptions.AspNetCore;
 
@@ -26,38 +20,20 @@ public static class SetupExtensions
 	public static WebApplication Setup (this WebApplicationBuilder builder)
 	{
 		builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-		builder.AddReflexConfiguration();
-		builder.AddReflexLogging();
 
-		builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-		builder.Services.AddControllers();
+		builder.AddReflexCore((_, container) => { container.RegisterModule<CoreModule>(); });
 
 		var assemblies = AppDomain.CurrentDomain.GetIncludedAssemblies().ToList();
 		var configuration = builder.Configuration;
 
 		builder.Services.AddGraphQl(assemblies);
-		builder.Services.AddCache();
-		builder.Services.AddValidation(assemblies);
 		builder.Services.AddDatabase<MainDb>(configuration);
 		builder.Services.AddIdentityModule(configuration);
-
-		builder.Services.AddCors();
-		builder.Services.AddHttpContextAccessor();
-		builder.Services.AddMvc()
-			.AddControllersAsServices()
-			.UseSlugRoutes()
-			.AddHttpExceptions();
-		builder.Services.AddFluentValidationAutoValidation()
-			.AddFluentValidationClientsideAdapters();
 
 		builder.Services.AddSwaggerGen(
 			c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotnetStarter", Version = "v1" }); }
 		);
-
-
-		builder.Host.ConfigureContainer<ContainerBuilder>(container => { container.RegisterModule<CoreModule>(); });
-
+		
 		var app = builder.Build();
 
 		app.UseRouting();
